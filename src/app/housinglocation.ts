c13d12091148441c5e4d73f702e6a2ce6ecdf9e8
Sergey Korsensky <sergey.korsensky@intel.com>
2024-05-19 13:05:23 +0300

    Update housinglocation

--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -1,2 +1,11 @@
-export interface Housinglocation {
+export interface HousingLocation {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
 }
+
